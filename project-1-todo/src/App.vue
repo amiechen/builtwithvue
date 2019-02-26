cb08d93f8c7b77f3729dfdbb46ed16a74173405e
Amie Chen <chinghc@gmail.com>
2019-02-26 18:47:01 +0800

    setup vue and tailwind

---
+++ project-1-todo/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "app",
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+<style>
+#app {
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ project-1-todo/src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1 class="bg-purple text-white">{{msg}}</h1>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      msg: "hihihihihi"
+    };
+  }
+};
+</script>
